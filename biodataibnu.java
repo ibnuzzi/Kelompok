ec9fdf5ac1329a825d35551de272b484569520c4
izza <muhammadibnzzi@gmail.com>
2024-09-05 11:13:27 +0700

    commit biodata

---
+++ biodataibnu.java
@@ -0,0 +1,13 @@
+public class biodataibnu {
+
+    public static void main(String[] args) {
+        System.out.println("Nama : Muhammad Ibnu Zauzi");
+        System.out.println("NIM : 244107020016");
+        System.out.println("Kelas : 1D");
+        System.out.println("No Absen : 19");
+        System.out.println("Prodi : Teknik Informatika");
+        System.out.println("Alamat : Dsn Jambu, Kec. Tugu, Kab. Trenggalek");
+        System.out.println("No Hp : 081334213262");
+        System.out.println("Hobi : Basket");
+    }
+}
