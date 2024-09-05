eb3073d5f632ef6bc2cc75ceec46d66e7632c4a8
Marcel-12345 <116170283+Marcel-12345@users.noreply.github.com>
2024-09-05 11:56:47 +0700

    Add files via upload

---
+++ BiodataMarcel.java
@@ -0,0 +1,13 @@
+public class BiodataMarcel {
+
+    public static void main(String[] args) {
+        System.out.println("Nama     : Anselmus Marcel Putra Andria");
+        System.out.println("NIM      : 244107020141");
+        System.out.println("Kelas    : 1D");
+        System.out.println("No Absen : 5");
+        System.out.println("Prodi    : Teknik Informatika");
+        System.out.println("Alamat   : Jalan Bogor Gang 1 No. 95");
+        System.out.println("No Hp    : 081321176453");
+        System.out.println("Hobi     : Main Game");
+    }
+}
